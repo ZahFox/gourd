c1eb915f6650fcd864d29623f7947ce4619a5631
ZahFox <ZahFoxTales@gmail.com>
2019-11-25 19:57:05 -0600

    feat(utils): added a new file function

--- pkg/utils/file.go
+++ pkg/utils/file.go
@@ -7,6 +7,37 @@ import (
 	"os"
 )
 
+// FileError is used to tell the difference between different kind of file errors
+type FileError int
+
+const (
+	// FENONE means there is no file error
+	FENONE FileError = iota
+	// FENOEXIST means a file or directory does not exist
+	FENOEXIST
+	// FEINSFPERM means the active user has insufficient permissions for the
+	// requested action on the file
+	FEINSFPERM
+)
+
+// Exists returns whether the given file or directory exists
+func Exists(path string) (bool, FileError) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, FENONE
+	}
+
+	if os.IsNotExist(err) {
+		return false, FENOEXIST
+	}
+
+	if os.IsPermission(err) {
+		return true, FEINSFPERM
+	}
+
+	return true, FENONE
+}
+
 // MkdirIfNotExist is like mkdir -p
 func MkdirIfNotExist(dir string) {
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
